c37b623284efc711e3a7733978abb39db770fb0e
chagn <ycw99@hotmail.com>
2020-09-18 14:07:56 +0800

    1.0 version

---
+++ lib/omniauth/money/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Money
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/omniauth/money_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Omniauth::Money do
+  it "has a version number" do
+    expect(Omniauth::Money::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
